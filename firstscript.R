2b304c815964da0cfa86a6d11f370e1427194af2
Bots <david.bots@philips.com>
2020-04-29 16:12:55 +0200

    explanation on connecting and pushing data to github; course - data science: productivity tools on EDX.org -> video 1.3 introduction to git and github

--- firstscript.R
+++ firstscript.R
@@ -1,2 +1,15 @@
-## bitch please test me
-library(tidyverse)
+## execute the following steps to connect to github
+# in terminal execute the following code: git config --global user.name "Bots"
+# in terminal execute the following code: git config --gloabl user.mail "david.bots@philips.com"
+# --> please make sure the text is 100% correct
+
+# 1. create new repo in github (username DB0510)
+# 2. copy link of new repo
+# 3. in Rstudio; create a new project - with version control
+# 4. paste the github link
+# use terminal in Rstudio: and write code
+# first commit in git (top right pane)
+# then push to github
+
+
+
